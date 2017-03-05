eab41a08937dcc3bc030ab72d8a932e00bc59bef
Jeppe Reinhold <jeppereinhold@gmail.com>
2017-03-05 17:51:15 +0100

    Stop computing iOS shadows on Android (#3)
    * don't compute ios shadows on android
    make constant variables const instead of const
    don't overwrite shadowColor in switch-statement
    
    * added proptypes for elevation prop. fixed code-style in returns

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ import React from 'react';
 import { View, Platform } from 'react-native';
 
 export default class ElevatedView extends React.Component {
+  static propTypes = {
+    //elevation of 0 results in a default View.
+    elevation: React.PropTypes.oneOf([0, 1, 2, 3, 4, 5])
+  }
+  static defaultProps = {
+    elevation: 0
+  }
+
   render() {
-    let { elevation, style, ...otherProps } = this.props;
-    let iosShadowElevation = {
-      shadowColor: 'black',
-    };
+    const { elevation, style, ...otherProps } = this.props;
+
+    if (Platform.OS === 'android') {
+      return (
+        <View elevation={elevation} style={style} {...otherProps}>
+          {this.props.children}
+        </View>
+      );
+    }
+
+    let iosShadowElevation = {};
 
     switch (elevation) {
       case 1:
@@ -58,13 +73,12 @@ export default class ElevatedView extends React.Component {
         break;
     }
 
-    return Platform.select({
-      ios: <View style={[iosShadowElevation, style]} {...otherProps}>
+    iosShadowElevation.shadowColor = 'black';
+
+    return (
+      <View style={[iosShadowElevation, style]} {...otherProps}>
         {this.props.children}
-      </View>,
-      android: <View elevation={elevation} style={style} {...otherProps}>
-        {this.props.children}
-      </View>,
-    });
+      </View>
+    );
   }
 }
