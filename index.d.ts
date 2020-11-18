58a5c5990b675b3f553f2d5014ab6b91a2968224
Nico Montanari <nicomontanari.98@gmail.com>
2020-11-18 13:12:34 +0100

    Created the type file

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+import {
+    Component
+} from 'react'
+import {
+    ViewProps
+} from 'react-native'
+
+interface Props extends ViewProps {
+    elevation: number
+}
+
+export default class ElevatedView extends Component<Props> {
+    
+}
